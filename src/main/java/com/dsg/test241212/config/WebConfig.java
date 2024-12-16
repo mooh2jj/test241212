21abd69726350fd68c51da8640f4312c15e4780d
seonggon do <mooh2jj@naver.com>
2024-12-16 09:58:52 +0900

    WebConfig Cors 설정 추가

---
+++ src/main/java/com/dsg/test241212/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.dsg.test241212.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(300)    // 300초 동안 캐싱
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+    }
+
+
+
+}
