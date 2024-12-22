8d9da0cfe1d082470e9357625c2911efaf66b75b
seonggon do <mooh2jj@naver.com>
2024-12-22 15:14:55 +0900

    DotEnvConfig 파일 수정

--- src/main/java/com/dsg/test241212/config/DotEnvConfig.java
+++ src/main/java/com/dsg/test241212/config/DotEnvConfig.java
@@ -10,6 +10,8 @@ public class DotEnvConfig {
     @Bean
     public Dotenv dotenv() {
         // .env 파일을 읽어서 환경변수로 사용
-        return Dotenv.load();
+        return Dotenv.configure().directory("./")
+                .ignoreIfMissing() // .env 파일이 없어도 에러 발생 안함
+                .load();
     }
 }
